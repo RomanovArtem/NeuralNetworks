a96d5a35491d3b18c6cadd55c1c409c33d44825e
timon <a.k.romanov@mail.ru>
2017-09-13 00:39:54 +0300

    Добавил класс Point

---
+++ NeuralNetworksLab1/Point.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworksLab1
+{
+    public class Point
+    {
+        public double X;
+        public double Y;
+        public int Class;
+
+        public Point()
+        {
+
+        }
+
+        public Point(double x, double y, int type)
+
+        {
+            X = x;
+            Y = y;
+            Class = type;
+        }
+    }
+}

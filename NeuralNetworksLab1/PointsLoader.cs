da75c745d6349ecc5e58b0dcff0f4485b83dad47
timon <a.k.romanov@mail.ru>
2017-09-13 00:48:05 +0300

    Добавил класс PointsLoader

---
+++ NeuralNetworksLab1/PointsLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace NeuralNetworksLab1
+{
+    public class PointsLoader
+    {
+        public static List<Point> GetPoints(string path)
+        {
+            var points = new List<Point>();
+
+            using (var file = new StreamReader(File.OpenRead(path)))
+            {
+                var line = file.ReadLine();
+                while (line != null)
+                {
+                    var numbers = line.Split(' ');
+                    var x = double.Parse(numbers[0]);
+                    var y = double.Parse(numbers[1]);
+                    var type = int.Parse(numbers[2]);
+
+                    var point = new Point(x, y, type);
+                    points.Add(point);
+                    line = file.ReadLine();
+                }
+            }
+            return points;
+        }
+
+    }
+}
